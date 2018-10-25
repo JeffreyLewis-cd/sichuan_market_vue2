e65c7fc068eac4d7c57a1610b4ad1eeeefe3293c
FJiang6 <jiangfan6@outlook.com>
2018-10-25 22:32:33 +0800

    image

--- src/components/productDataManage/productDataManage.vue
+++ src/components/productDataManage/productDataManage.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="productDataManagePage">
-    <p>"productDataManage"</p>
+    <div class="productsCategory">
+      <span>产品分类：</span>
+      <el-button :type="(item.value===activeCategory)?'primary':'text'" size="small" plain
+                 v-for="(item,index) in proCategoryList" :key="index"
+                 @click.native="switchProCategory(item.value)" class="proCateItem"> {{item.label}}
+      </el-button>
+    </div>
+    <div class="productsList">
+      <div class="productsItem">
+
+      </div>
+    </div>
   </div>
 
 </template>
@@ -9,7 +20,35 @@
   export default {
     name: "productDataManage",
     data() {
-      return {}
+      return {
+        proCategoryList: [
+          {
+            label: "粮食",
+            value: "401"
+          },
+          {
+            label: "蔬菜",
+            value: "402"
+          },
+          {
+            label: "油料",
+            value: "403"
+          },
+          {
+            label: "水果",
+            value: "404"
+          },
+          {
+            label: "花卉",
+            value: "405"
+          },
+          {
+            label: "调料",
+            value: "406"
+          },
+        ],
+        activeCategory: "401"
+      }
     },
     mounted() {
 
@@ -17,11 +56,44 @@
 
     components: {},
 
-    methods: {},
+    methods: {
+      /*切换产品分类*/
+      switchProCategory(value) {
+          this.activeCategory=value;
+      }
+    },
 
   }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+  .productDataManagePage {
+    .productsCategory {
+      height: 36px;
+      width: 100%;
+      border: 1px solid gray;
+      .proCateItem {
+        margin-right: 20px;
+        letter-spacing: 5px;
+        /*border:1px solid red;*/
+        padding: 6px 20px;
+        font-weight: bold;
+      }
+    }
+    .productsList {
+      min-height: 500px;
+      width: 100%;
+      border: 1px solid gray;
+    }
 
+  }
+
+
+</style>
+<style lang="less">
+  .productsCategory {
+    .el-button--text {
+      color: rgba(0, 0, 0, .65);
+    }
+  }
 </style>
