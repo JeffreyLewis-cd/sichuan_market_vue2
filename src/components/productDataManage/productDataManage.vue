d083ae359f541b01387f7585878ee1df001c0e85
FJiang6 <jiangfan6@outlook.com>
2018-10-27 17:59:00 +0800

    images02

--- src/components/productDataManage/productDataManage.vue
+++ src/components/productDataManage/productDataManage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="productDataManagePage">
+    <!--产品分类-->
     <div class="productsCategory">
       <span>产品分类：</span>
       <el-button :type="(item.value===activeCategory)?'primary':'text'" size="small" plain
@@ -7,9 +8,15 @@
                  @click.native="switchProCategory(item.value)" class="proCateItem"> {{item.label}}
       </el-button>
     </div>
-    <div class="productsList">
-      <div class="productsItem">
-
+    <!--产品列表卡片-->
+    <div class="productsContent">
+      <div class="productsItem" v-for="(proItem,index) in productsList" :key="index">
+        <!--<img src="../../assets/image/img01/lemon_01.png" class="proThumbnail"/>-->
+        <img :src="proItem.productThumbnail" class="proThumbnail"/>
+        <div class="proText">
+          <h4>{{proItem.productName}}</h4>
+          <p>{{proItem.productDescribe}}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -23,11 +30,11 @@
       return {
         proCategoryList: [
           {
-            label: "粮食",
+            label: "水果",
             value: "401"
           },
           {
-            label: "蔬菜",
+            label: "粮食",
             value: "402"
           },
           {
@@ -35,7 +42,7 @@
             value: "403"
           },
           {
-            label: "水果",
+            label: "蔬菜",
             value: "404"
           },
           {
@@ -47,7 +54,77 @@
             value: "406"
           },
         ],
-        activeCategory: "401"
+        activeCategory: "401",
+        productsList: [
+          {
+            productId: "",
+            productName: '柠檬',
+            productDescribe: '柠檬描述',
+            productThumbnail: require("../../assets/image/products/lemon02.jpg"),
+            productDetails:{
+              images:[
+                {
+                  txt:"",
+                  url:"",
+                },
+                {
+                  txt:"",
+                  url:"",
+                },
+                {
+                  txt:"",
+                  url:"",
+                },
+              ],
+              videos:[
+                {
+                  txt:"",
+                  url:"",
+                }
+              ]
+            }
+          },
+          {
+            productId: "",
+            productName: '水蜜桃',
+            productDescribe: '水蜜桃描述',
+            productThumbnail: require("../../assets/image/products/honeyPeach02.jpg"),
+            productImage: [],
+            productVideo: []
+          },
+          {
+            productId: "",
+            productName: '猕猴桃',
+            productDescribe: '猕猴桃描述',
+            productThumbnail: require("../../assets/image/products/kiwifruit01.jpg"),
+            productImage: [],
+            productVideo: []
+          },
+          {
+            productId: "",
+            productName: '芒果',
+            productDescribe: '芒果描述',
+            productThumbnail: require("../../assets/image/products/mango01.jpg"),
+            productImage: [],
+            productVideo: []
+          },
+          {
+            productId: "",
+            productName: '石榴',
+            productDescribe: '石榴描述',
+            productThumbnail: require("../../assets/image/products/pomegranate01.jpg"),
+            productImage: [],
+            productVideo: []
+          },
+          {
+            productId: "",
+            productName: '草莓',
+            productDescribe: '草莓描述',
+            productThumbnail: require("../../assets/image/products/strabarrey01.jpg"),
+            productImage: [],
+            productVideo: []
+          },
+        ],
       }
     },
     mounted() {
@@ -59,7 +136,7 @@
     methods: {
       /*切换产品分类*/
       switchProCategory(value) {
-          this.activeCategory=value;
+        this.activeCategory = value;
       }
     },
 
@@ -68,22 +145,47 @@
 
 <style scoped lang="less">
   .productDataManagePage {
+
     .productsCategory {
       height: 36px;
-      width: 100%;
-      border: 1px solid gray;
+      width: calc(100% - 60px);
+      border-bottom: 1px solid #E4E7ED;
+      margin-left: 30px;
       .proCateItem {
         margin-right: 20px;
         letter-spacing: 5px;
-        /*border:1px solid red;*/
         padding: 6px 20px;
         font-weight: bold;
       }
     }
-    .productsList {
-      min-height: 500px;
-      width: 100%;
-      border: 1px solid gray;
+    .productsContent {
+      min-height: 460px;
+      height: calc(100vh - 250px);
+      width: calc(100% - 50px);
+      padding: 10px;
+      margin-top: 15px;
+      overflow: auto;
+      .productsItem {
+        height: 360px;
+        width: calc(23% - 4px);
+        overflow: hidden;
+        background-color: #E8F2FA;
+        margin: 20px 1%;
+        border: 1px solid rgba(211, 229, 247, 0.5);
+        border-radius: 3px;
+        cursor: pointer;
+        float: left;
+        .proThumbnail {
+          height: 200px;
+          width: 100%;
+        }
+        .proText {
+          padding: 15px;
+        }
+      }
+      .productsItem:hover {
+        box-shadow: 0 0 20px rgba(160, 160, 160, 0.9);
+      }
     }
 
   }
